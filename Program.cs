66cfc04f09d70c90136da1cd586dd635c5aa831e
RickBraz <61334944+RickBraz@users.noreply.github.com>
2020-06-26 19:24:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,19 +5,7 @@ namespace Media4Notas
     class Program
     {
         static void Main(string[] args)
-        {
-     /*  [console: Media4Notas] Receba 4 notas, com somente uma casa decimal.
-     Valide se todas estão entre 0.0 e 10.0.
-     Se alguma delas não estiver, exiba "Digite somente notas entre 0 e 10.".
-     Caso todas as notas sejam válidas, calcule a média aritmética das notas.
-     Exiba uma mensagem no seguinte padrão: "Você ficou com média 7,5. Resultado: Aprovado".
-
-Resultados possíveis:
-
-"Reprovado" para médias menores que 5.0
-"Em recuperação" para médias entre 5.0 e 6.0
-"Aprovado", para médias acima de 6.0*/
-            
+        {          
             Console.WriteLine("Media4Notas, digite 4 numeros");
             Console.Write("Digite o primeiro numero: ");
             decimal a = Convert.ToDecimal(Console.ReadLine());
